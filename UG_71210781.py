55cb34df6adc84158ccdc79817587e86f1c20e9f
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2022-09-07 18:58:45 +0700

    first commit

---
+++ UG_71210781.py
@@ -0,0 +1,9 @@
+# def deret_ajaib(n):
+#     data = []
+#     for i in range(1, n+1):
+#         if i > 5:
+#             data.append((i-2)+(i//2))
+#         else:
+#             data.append(i)
+#     return data
+# print(deret_ajaib(10))
